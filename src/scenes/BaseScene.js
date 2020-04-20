00fc27ba22ef39b715c432142edda1e146e359a7
Kevin <kevin.isom@gmail.com>
2020-04-20 16:55:49 +1200

    clean up

--- src/scenes/BaseScene.js
+++ src/scenes/BaseScene.js
@@ -1,6 +1,9 @@
 /* eslint-disable spellcheck/spell-checker */
 import Phaser from 'phaser'
 import Player, { PLAYER_KEY, PLAYER_ANIMS } from '../entities/player'
+import Skeleton from '../entities/skeleton'
+import Hellcat from '../entities/hellcat'
+import HellCat from '../entities/hellcat'
 
 const DEATH_ANIM_KEY = 'please-die'
 
@@ -12,7 +15,11 @@ const ENEMY_KEYS = {
   WOLF: 'wolf',
   DIE: 'death',
 }
-
+const ENEMY_HP = {
+  [ENEMY_KEYS.SKELETON_BOSS]: 5,
+  [ENEMY_KEYS.SKELETON]: 1,
+  [ENEMY_KEYS.HELLCAT]: 1,
+}
 export default class BaseScene extends Phaser.Scene {
   constructor(args) {
     super(args)
@@ -22,15 +29,20 @@ export default class BaseScene extends Phaser.Scene {
     return new Player(this, 100, 150)
   }
 
-  createSkeleton(x, y) {
-    console.info('createSkeleton BASE LEVEL')
-    const skeleton = this.physics.add.sprite(x, y, ENEMY_KEYS.SKELETON)
+  //
+  createSkeleton(x, y, key = ENEMY_KEYS.SKELETON) {
+    new Skeleton(this, x, y, key)
+  }
 
+  createSkeletonOld(x, y, key = ENEMY_KEYS.SKELETON) {
+    const skeleton = this.physics.add.sprite(x, y, key)
+    skeleton.KEY = key
+    skeleton.HP = ENEMY_HP[key]
     skeleton.setCollideWorldBounds(true)
 
     this.anims.create({
-      key: 'walk',
-      frames: this.anims.generateFrameNumbers(ENEMY_KEYS.SKELETON, {
+      key: `${key}-walk`,
+      frames: this.anims.generateFrameNumbers(key, {
         start: 0,
         end: 7,
       }),
@@ -39,8 +51,8 @@ export default class BaseScene extends Phaser.Scene {
     })
 
     this.anims.create({
-      key: 'stay',
-      frames: this.anims.generateFrameNumbers(ENEMY_KEYS.SKELETON, {
+      key: `${key}-stay`,
+      frames: this.anims.generateFrameNumbers(key, {
         start: 8,
         end: 9,
       }),
@@ -48,8 +60,8 @@ export default class BaseScene extends Phaser.Scene {
       repeat: -1,
     })
     this.anims.create({
-      key: 'rise',
-      frames: this.anims.generateFrameNumbers(ENEMY_KEYS.SKELETON, {
+      key: `${key}-rise`,
+      frames: this.anims.generateFrameNumbers(key, {
         start: 8,
         end: 14,
       }),
@@ -62,17 +74,10 @@ export default class BaseScene extends Phaser.Scene {
     skeleton.body.setOffset(10, 43)
 
     const animComplete = function (event, character, deets) {
-      console.info('Skeleton Animation Complete', event)
-      if (event.key === 'rise') {
-        skeleton.anims.play('walk', true)
+      if (event.key === `${key}-rise`) {
+        skeleton.anims.play(`${key}-walk`, true)
         skeleton.body.setSize()
         skeleton.setVelocityX(-10)
-      }
-      if (event.key === DEATH_ANIM_KEY) {
-        console.info('Pause Now Please')
-        // this.scene.pause()
-        console.info('Die Now Please')
-        skeleton.destroy()
       }
     }
 
@@ -82,35 +87,7 @@ export default class BaseScene extends Phaser.Scene {
   }
 
   createHellcat(x, y) {
-    const hellcat = this.physics.add.sprite(x, y, ENEMY_KEYS.HELLCAT)
-
-    hellcat.setCollideWorldBounds(true)
-
-    this.anims.create({
-      key: 'catwalk',
-      frames: this.anims.generateFrameNumbers(ENEMY_KEYS.HELLCAT, {
-        start: 0,
-        end: 3,
-      }),
-      frameRate: 10,
-      repeat: -1,
-    })
-
-    const animComplete = function (event, character, deets) {
-      console.info('Hellcat Animation Complete', event)
-      if (event.key === DEATH_ANIM_KEY) {
-        console.info('Hellcat Die Now Please')
-        try {
-          hellcat.destroy()
-        } catch (err) {
-          console.error('destroy fail', err)
-        }
-      }
-    }
-
-    hellcat.on('animationcomplete', animComplete, this)
-
-    return hellcat
+    new HellCat(this, x, y, ENEMY_KEYS.HELLCAT)
   }
 
   preload() {
@@ -148,7 +125,27 @@ export default class BaseScene extends Phaser.Scene {
     })
   }
 
-  dieNow(gameObject) {
+  playerHit(player, enemy) {
+    console.info('player hit')
+
+    if (
+      player.anims.currentAnim.key === PLAYER_ANIMS.ATTACK1 ||
+      player.anims.currentAnim.key === PLAYER_ANIMS.ATTACK2 ||
+      player.anims.currentAnim.key === PLAYER_ANIMS.ATTACK3
+    ) {
+      this.dieNow(enemy)
+    } else {
+      player.body.setVelocity(0, 0)
+      player.setX(player.x - 50)
+
+      player.play(PLAYER_ANIMS.HIT, true)
+
+      enemy.body.setVelocity(0, 0)
+      enemy.setX(enemy.x - 50)
+    }
+  }
+
+  dieNow2(gameObject) {
     const boom = function (event, character, deets) {
       console.info('Kah BOOM?', event)
       if (event.key === DEATH_ANIM_KEY) {
@@ -164,7 +161,35 @@ export default class BaseScene extends Phaser.Scene {
     gameObject.play(DEATH_ANIM_KEY)
   }
 
-  create() { }
+  dieNow(gameObject) {
+    const boom = function (event, character, deets) {
+      console.info('Kah BOOM?', event)
+      if (event.key === DEATH_ANIM_KEY) {
+        gameObject.destroy()
+      }
+    }
+    gameObject.body.setVelocityX(0)
+
+    gameObject.setTexture(ENEMY_KEYS.DIE)
+
+    console.info('fall down, go boom')
+
+    gameObject.play(DEATH_ANIM_KEY)
+    gameObject.once('animationcomplete', boom, this)
+  }
+
+  create() {
+    this.input.gamepad.on(
+      'down',
+      function (pad, button, index) {
+        if (button.index === 9) {
+          this.scene.launch('pause-scene', { pausedScene: this.scene.key })
+          this.scene.pause()
+        }
+      },
+      this
+    )
+  }
 
   update() {
     if (this.player.x > 256 * 16 - 100) {
