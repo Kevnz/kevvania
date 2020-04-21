3feb794785a1198216867b02f9faa908fb2c6a0e
Kevin <kevin.isom@gmail.com>
2020-04-21 14:51:58 +1200

    make better

--- src/entities/hellcat.js
+++ src/entities/hellcat.js
@@ -1,10 +1,10 @@
 import Phaser from 'phaser'
-import { ENEMY_KEYS, DEATH_ANIM_KEY } from '../utils/constants'
+import { ENEMY_KEYS, ENEMY_HP } from '../utils/constants'
 
 export default class HellCat extends Phaser.GameObjects.Sprite {
   constructor(scene, x, y) {
     super(scene, x, y, ENEMY_KEYS.HELLCAT)
-    // this.setCollideWorldBounds(true)
+
     scene.add.existing(this)
     scene.physics.world.enableBody(this)
     this.body.setCollideWorldBounds(true)
@@ -12,7 +12,7 @@ export default class HellCat extends Phaser.GameObjects.Sprite {
     scene.enemies.add(this)
 
     this.KEY = ENEMY_KEYS.HELLCAT
-    this.HP = 1 // ENEMY_HP[key]
+    this.HP = ENEMY_HP[ENEMY_KEYS.HELLCAT]
 
     scene.anims.create({
       key: 'catwalk',
@@ -24,58 +24,81 @@ export default class HellCat extends Phaser.GameObjects.Sprite {
       repeat: -1,
     })
 
-    const animComplete = function (event, character, deets) {
-      console.info('Hellcat Animation Complete', event)
-    }
-
-    this.on('animationcomplete', animComplete, this)
-
     scene.enemies.add(this)
   }
 
-  update(player) {
-    // console.log('here kitty kitty')
-    if (this === null) {
-      console.log('no kitty')
-      return
-    }
-    if (this.anims === undefined) {
-      console.log('no kitty')
-      return
-    }
+  ifNoAnimationPlay() {
     if (!this.anims.currentAnim) {
-      console.log('stay')
       this.anims.play(`catwalk`, true)
     }
+  }
+
+  ifPlayerIsCloseOnTheLeft(distance, player) {
+    if (distance < 250 && this.x > player.x && distance > 125) {
+      console.info('ifPlayerIsCloseOnTheLeft')
+      this.body.setVelocityX(-15)
+      this.flipX = false
+    }
+  }
+
+  ifPlayerIsCloseOnTheRight(distance, player) {
+    if (distance < 250 && this.x < player.x && distance > 125) {
+      console.info('ifPlayerIsCloseOnTheRight')
+      this.body.setVelocityX(15)
+      this.flipX = true
+    }
+  }
+
+  ifPlayerIsNearOnTheLeft(distance, player) {
+    if (distance < 125 && this.x > player.x) {
+      console.info('ifPlayerIsNearOnTheLeft')
+      this.body.setVelocityX(-55)
+
+      this.flipX = false
+    }
+  }
+
+  ifPlayerIsNearOnTheRight(distance, player) {
+    if (distance < 125 && this.x < player.x) {
+      this.body.setVelocityX(55)
+      this.flipX = true
+    }
+  }
+
+  ifPlayerIsComing(distance, player) {
+    if (distance < 350 && distance > 250 && this.x > player.x) {
+      this.body.setVelocityX(-45)
+    } else if (distance < 350 && distance > 250 && this.x < this.player.x) {
+      this.body.setVelocityX(45)
+      this.body.flipX = true
+    }
+  }
+
+  update(player) {
+    if (this === null) {
+      return
+    }
+    if (this.DEAD) return
+    if (this.anims === undefined) {
+      return
+    }
+    this.ifNoAnimationPlay()
     const dist = Phaser.Math.Distance.Between(
       player.x,
       player.y,
       this.x,
       this.y
     )
-    if (dist < 250 && this.x > player.x && dist > 125) {
-      this.body.setVelocityX(-15)
-      this.flipX = false
-    } else if (dist < 2500 && this.x < player.x && dist > 125) {
-      this.body.setVelocityX(15)
-      this.flipX = true
-    } else if (dist < 125 && this.x > player.x) {
-      this.body.setVelocityX(-55)
-      this.flipX = false
-    } else if (this.x < player.x && dist < 125) {
-      this.body.setVelocityX(55)
-      this.flipX = true
-    } else {
-      this.body.setVelocityX(0)
-    }
 
-    if (dist < 350 && this.x > this.player.x) {
-      console.info('here kitty kitty')
-      this.anims.play('catwalk', true)
-      this.body.setVelocityX(-45)
-    } else if (dist < 350 && this.x < this.player.x) {
-      this.body.setVelocityX(45)
-      this.body.flipX = true
+    this.ifPlayerIsComing(dist, player)
+
+    this.ifPlayerIsNearOnTheLeft(dist, player)
+    this.ifPlayerIsNearOnTheRight(dist, player)
+
+    this.ifPlayerIsCloseOnTheLeft(dist, player)
+    this.ifPlayerIsCloseOnTheRight(dist, player)
+    if (dist > 350) {
+      this.body.setVelocityX(0)
     }
   }
 }
