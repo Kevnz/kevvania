308feca83a71dbe5193d842f435e6c62c66dbfe9
Kevin <kevin.isom@gmail.com>
2020-04-25 16:59:33 +1200

    More

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,10 +1,12 @@
 export const FULL_WIDTH = 256 * 16
 export const SCREEN_WIDTH = 32 * 16
 export const FULL_HEIGHT = 16 * 16
+export const SCREEN_HEIGHT = 16 * 16
 
 export const MAIN_BACKGROUND = 'main-background'
 export const MAP_KEY = 'map'
 export const LEVEL_2_MAP_KEY = 'level-2-map'
+export const LEVEL_3_MAP_KEY = 'level-3-map'
 export const CEMETERY_OBJECTS_KEY = 'cemetery-objects'
 export const CEMETERY_TILES_KEY = 'cemetery-tiles'
 export const ENV_TILES_KEY = 'env-tiles'
@@ -13,6 +15,10 @@ export const TWILIGHT_BW_TILES = 'twilight-bw-tiles'
 export const TWILIGHT_TILES = 'twilight-tiles'
 export const STONE_ANGEL_KEY = 'stone-angel'
 export const CASTLE_EXTERIOR_KEY = 'castle_tileset_part1'
+export const MAIN_CASTLE = 'main-castle'
+export const MAIN_CASTLE_DECORATIVE = 'decorative'
+export const MAIN_CASTLE_ENVIRONMENT = 'environment-objects'
+export const MAIN_CASTLE_WOOD = 'wood'
 
 export const DEATH_ANIM_KEY = 'please-die'
 
@@ -24,7 +30,7 @@ export const ENEMY_KEYS = {
   WOLF: 'wolf',
   DIE: 'death',
   FLAMING_SKULL: 'flaming-skull',
-  SKELETON_SWORD: 'skeleton-sword',
+  SKELETON_WARRIOR: 'skeleton-sword',
   BANDIT: 'bandit',
 }
 export const ENEMY_HP = {
@@ -33,4 +39,5 @@ export const ENEMY_HP = {
   [ENEMY_KEYS.HELLCAT]: 2,
   [ENEMY_KEYS.FLAMING_SKULL]: 2,
   [ENEMY_KEYS.BANDIT]: 3,
+  [ENEMY_KEYS.SKELETON_WARRIOR]: 4,
 }
