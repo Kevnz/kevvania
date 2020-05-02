3fbea3192cb54a87285ae1cbcaa14d7fba650ec1
Kevin <kevin.isom@gmail.com>
2020-05-02 13:03:23 +1200

    handle music on pause and font update

--- src/scenes/PauseScene.js
+++ src/scenes/PauseScene.js
@@ -8,6 +8,11 @@ export default class PauseScene extends Phaser.Scene {
 
   create({ pausedScene }) {
     console.info('pause create', pausedScene)
+    if (this.music) {
+      this.music.stop()
+    } else {
+      console.info('no music')
+    }
     this.input.gamepad.on(
       'down',
       function (pad, button, index) {
@@ -15,6 +20,11 @@ export default class PauseScene extends Phaser.Scene {
         if (button.index === 9) {
           console.log('pause', this.scene)
           this.scene.resume(pausedScene)
+          if (this.scene.music) {
+            this.scene.music.resume()
+          } else {
+            console.info('no scene music')
+          }
           this.scene.stop()
         }
       },
@@ -22,7 +32,7 @@ export default class PauseScene extends Phaser.Scene {
     )
 
     this.add.text(110, 110, 'Paused', {
-      fontFamily: '"Times New Roman", Tahoma, serif',
+      fontFamily: 'deutsch_gothicnormal, "Times New Roman", Tahoma, serif',
       fontSize: 78,
       color: '#E70B91cc',
     })
