ace2b814af6dda675c84db46c3b1e247015891ea
Kevin <kevin.isom@gmail.com>
2020-05-02 13:04:25 +1200

    handle multiple scenes

--- src/scenes/ChangeScene.js
+++ src/scenes/ChangeScene.js
@@ -6,16 +6,18 @@ export default class IntroScene extends Phaser.Scene {
   constructor() {
     super('change-scene')
     this.background = null
+    this.nextScene = null
   }
 
   preload() {
     this.load.image(MAIN_BACKGROUND, 'assets/images/backgrounds/moon.png')
   }
 
-  create() {
+  create({ nextScene }) {
+    console.log('the next scene to load', nextScene)
     // this.background = this.add.image(0, 0, MAIN_BACKGROUND)
     // this.background.setDisplaySize(800, 600)
-
+    this.nextScene = nextScene
     const image = this.add.image(
       this.cameras.main.width / 2,
       this.cameras.main.height / 2,
@@ -26,7 +28,7 @@ export default class IntroScene extends Phaser.Scene {
     const scale = Math.max(scaleX, scaleY)
     image.setScale(scale).setScrollFactor(0)
     this.add.text(110, 110, 'Next', {
-      fontFamily: '"Times New Roman", Tahoma, serif',
+      fontFamily: 'deutsch_gothicnormal, "Times New Roman", Tahoma, serif',
       fontSize: 78,
       color: '#E70B91',
     })
@@ -35,7 +37,7 @@ export default class IntroScene extends Phaser.Scene {
 
   update() {
     if (this.cursors.space.isDown) {
-      this.scene.start('level-two')
+      this.scene.start(this.nextScene)
     }
     var pads = this.input.gamepad.gamepads
 
@@ -58,7 +60,8 @@ export default class IntroScene extends Phaser.Scene {
         console.info('down')
       }
       if (gamepad.A) {
-        this.scene.start('level-two')
+        console.log('start ', this.nextScene)
+        this.scene.start(this.nextScene)
       }
     }
   }
